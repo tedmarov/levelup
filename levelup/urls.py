51c1a4dbd464b71ba7517b4c3602513e40ea1e93
ted <tedmarov@gmail.com>
2021-02-06 00:20:27 -0600

    Client and Server work per chapter instructions

--- levelup/urls.py
+++ levelup/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from levelupapi.views import register_user, login_user
 
 urlpatterns = [
-    # Requests to http://localhost:8000/register will be routed to the register_user function
     path('register', register_user),
-    # Requests to http://localhost:8000/login will be routed to the login_user function
     path('login', login_user),
-    path(api-auth, include(rest_framework.urls, namespace=rest_framework)),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
